50be35c910a5569026c9cd6b2b5f8adf621562a3
FrijoMVP <135904860+FrijoMVP@users.noreply.github.com>
2025-07-30 22:09:18 -0300

    Update sesiondocente.php

--- Docente/sesiondocente.php
+++ Docente/sesiondocente.php
@@ -2,11 +2,9 @@
     session_start();
     $usuario = $_POST["usuario"];
     $contrasenia = $_POST["contrasenia"];
-    
     $login = "SELECT * FROM estudiantes WHERE usuario = '$usuario' AND contrasenia = '$contrasenia'";
     require '../enlace.php';
     $resultado = $mysqli->query($login);
-
 if ($resultado->num_rows > 0) {
     $fila = $resultado->fetch_assoc();
     $_SESSION['id'] = $fila['id']; 
